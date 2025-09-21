9e2ea38fb724787f54004a3fe84736ff7f0edb9f
Noor2Creative <noorahmedansari.1991@gmail.com>
2025-09-21 16:17:58 +0530

    test

--- MasjidConnectAPI/Controllers/MasjidController.cs
+++ MasjidConnectAPI/Controllers/MasjidController.cs
@@ -67,7 +67,6 @@ namespace MasjidConnectAPI.Controllers
                         // You can save file URL to DB here
                         var fileUrl = $"wwwroot/Images/Masjid";
 
-
                         sb.Append($"<Images><Image>{fileName}</Image><ImagePath>{fileUrl}</ImagePath></Images>");
                     }
                     sb.Append("</Document>");
